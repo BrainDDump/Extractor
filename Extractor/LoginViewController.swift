a0b057649d0cf10b7796f1bcca42e31b7b830d30
Rahul Shrestha <rahul.shrestha@selu.edu>
2016-02-27 15:17:10 -0800

    added UI for email login

--- Extractor/LoginViewController.swift
+++ Extractor/LoginViewController.swift
@@ -15,15 +15,25 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         decorateButton(facebookLoginButton,color: UIColor(red: 0.231, green: 0.349, blue: 0.596, alpha: 1.0) )
+        decorateButton(emailSignUpButton, color: UIColor(red: 0.333, green: 0.675, blue: 0.933, alpha: 1))
       
     }
     @IBOutlet weak var facebookLoginButton: UIButton!
     
+    
+    @IBOutlet weak var emailSignUpButton: UIButton!
+    
+ 
     private func decorateButton(button: UIButton, color: UIColor) {
         
-        button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        if(button==facebookLoginButton){
+            button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        } else {
+            button.setTitleColor(UIColor(red: 0.961, green: 0.231, blue: 0.231, alpha: 1), forState: UIControlState.Normal)
+        }
+        
         button.layer.borderColor = color.CGColor
-        button.backgroundColor = UIColor(red: 0.231, green: 0.349, blue: 0.596, alpha: 1.0)
+        button.backgroundColor = color;
         button.layer.borderWidth = 2
         button.layer.cornerRadius = 7
     }
