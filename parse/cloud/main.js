b691891d114ddd8d8e534fe33a95e604b0bd6aae
Kirill Dubovitskiy <kirill2003de@gmail.com>
2016-02-27 23:38:17 -0800

    Stable version

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,7 +1,32 @@
 
-const kMaxDepth    = 5;
+// Extentions
+Array.prototype.getUnique = function(){
+    var u = {}, a = [];
+    for(var i = 0, l = this.length; i < l; ++i){
+        if(u.hasOwnProperty(this[i])) {
+            continue;
+        }
+        a.push(this[i]);
+        u[this[i]] = 1;
+    }
+    return a;
+}
+
+// Configurations
+const kMaxDepth    = 3;
 const kMaxChildren = 2;
 
+const kTwillioCred = {
+    PHONE_NUMBER: "8055002188",
+
+    ACCOUNT_SID: "AC5fafcc7cd2a64bf9bd9cc1383b54ff79",
+    AUTH_TOKEN:  "ba6c1e90f165a4d12d87cca1633d788c"
+}
+
+// setup modules
+var twilio  = require('twilio')(kTwillioCred.ACCOUNT_SID, kTwillioCred.AUTH_TOKEN);
+
+// Helper functions
 var getFreeNode = function(callback) {
     var query = new Parse.Query("Node");
     query.lessThan("depth",            kMaxDepth);
@@ -15,16 +40,15 @@ var getFreeNode = function(callback) {
             }
 
             var randomNode = results[Math.floor(Math.random()* results.length)];
-
             callback.success(randomNode);
         },
         error: function(error) {
-            callback.error("Error in the function getFreeNode");
+            callback.error("Error in the function helpers.getFreeNode");
         }
     });
 }
 
-var getAncestors = function(node, callback) {
+var getUpperNodes = function(node, callback) {
     if (typeof(node.get("parent")) == "undefined") {
         callback(new Array());
         return;
@@ -33,7 +57,7 @@ var getAncestors = function(node, callback) {
     var ancestorQuery = new Parse.Query("Node");
     ancestorQuery.get(node.get("parent").id, {
         success: function(parent) {
-            getAncestors(parent, function(ancestors) {
+            getUpperNodes(parent, function(ancestors) {
                 var newArray = new Array(parent);
                 callback(ancestors.concat(newArray));
             });
@@ -41,7 +65,49 @@ var getAncestors = function(node, callback) {
         error: function(error) {
             callback(new Array("Error"));
         }
-    })
+    });
+}
+
+var getAllContributersIds = function(node, callback) {
+    getUpperNodes(node, function(nodes) {
+        var contributersIds = [];
+        nodes.forEach(function(item) {
+            contributersIds.push(item.get("owner").id);
+        });
+
+        callback(contributersIds.getUnique());
+    });
+}
+
+var sendTextToUser = function(userId, text) {
+    var nodeOwnerQuery = new Parse.Query("_User");
+    nodeOwnerQuery.get(userId, {
+        success: function(nodeOwner) {
+            if (nodeOwner.has("phoneNumber")) {
+                var phoneNumber = nodeOwner.get("phoneNumber");
+
+                console.log("phoneNumber");
+                console.log(phoneNumber);
+
+                twilio.sendSms({
+                    to:   phoneNumber,
+                    from: kTwillioCred.PHONE_NUMBER,
+                    body: text
+                },
+                {
+                    success: function(httpResponse) {
+                        console.log("SMS sent!");
+                    },
+                    error: function(httpResponse) {
+                        console.error("Uh oh, something went wrong");
+                    }
+                });
+            }
+        },
+        error: function(error) {
+            console.error(error);
+        }
+    });
 }
 
 Parse.Cloud.define("pull", function(request, response) {
@@ -52,7 +118,7 @@ Parse.Cloud.define("pull", function(request, response) {
                 return;
             }
 
-            getAncestors(node, function(ancestors) {
+            getUpperNodes(node, function(ancestors) {
                 var newArray = new Array(node);
 
                 node.increment("reservedChildren", 1);
@@ -75,11 +141,10 @@ Parse.Cloud.define("pull", function(request, response) {
 Parse.Cloud.define("reject", function(request, response) {
 
     var parentNodeId = request.params.objectId;
-    console.error(parentNodeId);
     var parentQuery = new Parse.Query("Node");
     parentQuery.get(parentNodeId, {
         success: function(parentNode) {
-            parentNode.increment("rating",            -1);
+            parentNode.increment("rating",           -1);
             parentNode.increment("reservedChildren", -1);
             parentNode.save({
                 success: function() {
@@ -97,31 +162,39 @@ Parse.Cloud.define("reject", function(request, response) {
 });
 
 Parse.Cloud.afterSave("Node", function(request, response) {
-    var newNode      = request.object;
-    var parentNodeId = newNode.get("parent").id
+    var newNode = request.object;
 
-    if (typeof(parentNodeId) == "undefined") {
-        response.success("Success afterSave. Root node created");
+    if (newNode.has("parent") == false) {
         return;
     }
 
+    if (newNode.get("depth") >= kMaxDepth) {
+        var userId = newNode.get("owner").id;
+
+        getUpperNodes(newNode, function(nodes) {
+            var text = newNode.get("content");
+            nodes.forEach(function(item) {
+                text = item.get("content") + " " + text;
+            });
+
+            getAllContributersIds(newNode, function(contributersIds) {
+                contributersIds.forEach(function(item) {
+                    sendTextToUser(item, text);
+                });
+            });
+        });
+    }
+
+    var parentNodeId = newNode.get("parent").id
     var parentQuery = new Parse.Query("Node");
     parentQuery.get(parentNodeId, {
         success: function(parentNode) {
             parentNode.addUnique("children", newNode.id);
-            parentNode.save({
-                success: function() {
-                    response.success();
-                },
-                error: function(error) {
-                    response.error(error);
-                }
-            });
+            parentNode.save();
         },
         error: function(error) {
-            response.error(error);
+            console.error(error);
         }
-    })
+    });
 });
-
 
